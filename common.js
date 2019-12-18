69535b2b60970496b87e8a309c14f9a974e67af5
Pulits <santiagomp11@hotmail.com>
2019-12-17 18:25:08 -0600

    Commit message

---
+++ common.js
@@ -0,0 +1,464 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["common"],{
+
+/***/ "./node_modules/@coreui/coreui-plugin-chartjs-custom-tooltips/dist/umd/custom-tooltips.js":
+/*!************************************************************************************************!*\
+  !*** ./node_modules/@coreui/coreui-plugin-chartjs-custom-tooltips/dist/umd/custom-tooltips.js ***!
+  \************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+(function (global, factory) {
+   true ? factory(exports) :
+  undefined;
+}(this, function (exports) { 'use strict';
+
+  /**
+   * --------------------------------------------------------------------------
+   * CoreUI Plugins - Custom Tooltips for Chart.js (v1.3.1): custom-tooltips.js
+   * Licensed under MIT (https://coreui.io/license)
+   * --------------------------------------------------------------------------
+   */
+  function CustomTooltips(tooltipModel) {
+    var _this = this;
+
+    // Add unique id if not exist
+    var _setCanvasId = function _setCanvasId() {
+      var _idMaker = function _idMaker() {
+        var _hex = 16;
+        var _multiplier = 0x10000;
+        return ((1 + Math.random()) * _multiplier | 0).toString(_hex);
+      };
+
+      var _canvasId = "_canvas-" + (_idMaker() + _idMaker());
+
+      _this._chart.canvas.id = _canvasId;
+      return _canvasId;
+    };
+
+    var ClassName = {
+      ABOVE: 'above',
+      BELOW: 'below',
+      CHARTJS_TOOLTIP: 'chartjs-tooltip',
+      NO_TRANSFORM: 'no-transform',
+      TOOLTIP_BODY: 'tooltip-body',
+      TOOLTIP_BODY_ITEM: 'tooltip-body-item',
+      TOOLTIP_BODY_ITEM_COLOR: 'tooltip-body-item-color',
+      TOOLTIP_BODY_ITEM_LABEL: 'tooltip-body-item-label',
+      TOOLTIP_BODY_ITEM_VALUE: 'tooltip-body-item-value',
+      TOOLTIP_HEADER: 'tooltip-header',
+      TOOLTIP_HEADER_ITEM: 'tooltip-header-item'
+    };
+    var Selector = {
+      DIV: 'div',
+      SPAN: 'span',
+      TOOLTIP: (this._chart.canvas.id || _setCanvasId()) + "-tooltip"
+    };
+    var tooltip = document.getElementById(Selector.TOOLTIP);
+
+    if (!tooltip) {
+      tooltip = document.createElement('div');
+      tooltip.id = Selector.TOOLTIP;
+      tooltip.className = ClassName.CHARTJS_TOOLTIP;
+
+      this._chart.canvas.parentNode.appendChild(tooltip);
+    } // Hide if no tooltip
+
+
+    if (tooltipModel.opacity === 0) {
+      tooltip.style.opacity = 0;
+      return;
+    } // Set caret Position
+
+
+    tooltip.classList.remove(ClassName.ABOVE, ClassName.BELOW, ClassName.NO_TRANSFORM);
+
+    if (tooltipModel.yAlign) {
+      tooltip.classList.add(tooltipModel.yAlign);
+    } else {
+      tooltip.classList.add(ClassName.NO_TRANSFORM);
+    } // Set Text
+
+
+    if (tooltipModel.body) {
+      var titleLines = tooltipModel.title || [];
+      var tooltipHeader = document.createElement(Selector.DIV);
+      tooltipHeader.className = ClassName.TOOLTIP_HEADER;
+      titleLines.forEach(function (title) {
+        var tooltipHeaderTitle = document.createElement(Selector.DIV);
+        tooltipHeaderTitle.className = ClassName.TOOLTIP_HEADER_ITEM;
+        tooltipHeaderTitle.innerHTML = title;
+        tooltipHeader.appendChild(tooltipHeaderTitle);
+      });
+      var tooltipBody = document.createElement(Selector.DIV);
+      tooltipBody.className = ClassName.TOOLTIP_BODY;
+      var tooltipBodyItems = tooltipModel.body.map(function (item) {
+        return item.lines;
+      });
+      tooltipBodyItems.forEach(function (item, i) {
+        var tooltipBodyItem = document.createElement(Selector.DIV);
+        tooltipBodyItem.className = ClassName.TOOLTIP_BODY_ITEM;
+        var colors = tooltipModel.labelColors[i];
+        var tooltipBodyItemColor = document.createElement(Selector.SPAN);
+        tooltipBodyItemColor.className = ClassName.TOOLTIP_BODY_ITEM_COLOR;
+        tooltipBodyItemColor.style.backgroundColor = colors.backgroundColor;
+        tooltipBodyItem.appendChild(tooltipBodyItemColor);
+
+        if (item[0].split(':').length > 1) {
+          var tooltipBodyItemLabel = document.createElement(Selector.SPAN);
+          tooltipBodyItemLabel.className = ClassName.TOOLTIP_BODY_ITEM_LABEL;
+          tooltipBodyItemLabel.innerHTML = item[0].split(': ')[0];
+          tooltipBodyItem.appendChild(tooltipBodyItemLabel);
+          var tooltipBodyItemValue = document.createElement(Selector.SPAN);
+          tooltipBodyItemValue.className = ClassName.TOOLTIP_BODY_ITEM_VALUE;
+          tooltipBodyItemValue.innerHTML = item[0].split(': ').pop();
+          tooltipBodyItem.appendChild(tooltipBodyItemValue);
+        } else {
+          var _tooltipBodyItemValue = document.createElement(Selector.SPAN);
+
+          _tooltipBodyItemValue.className = ClassName.TOOLTIP_BODY_ITEM_VALUE;
+          _tooltipBodyItemValue.innerHTML = item[0];
+          tooltipBodyItem.appendChild(_tooltipBodyItemValue);
+        }
+
+        tooltipBody.appendChild(tooltipBodyItem);
+      });
+      tooltip.innerHTML = '';
+      tooltip.appendChild(tooltipHeader);
+      tooltip.appendChild(tooltipBody);
+    }
+
+    var position = this._chart.canvas.getBoundingClientRect();
+
+    var positionY = this._chart.canvas.offsetTop;
+    var positionX = this._chart.canvas.offsetLeft;
+    var positionLeft = positionX + tooltipModel.caretX;
+    var positionTop = positionY + tooltipModel.caretY; // eslint-disable-next-line
+
+    var halfWidth = tooltipModel.width / 2;
+
+    if (positionLeft + halfWidth > position.width) {
+      positionLeft -= halfWidth;
+    } else if (positionLeft < halfWidth) {
+      positionLeft += halfWidth;
+    } // Display, position, and set styles for font
+
+
+    tooltip.style.opacity = 1;
+    tooltip.style.left = positionLeft + "px";
+    tooltip.style.top = positionTop + "px";
+  }
+
+  var customTooltips = CustomTooltips; // TODO: camel-case
+
+  exports.CustomTooltips = CustomTooltips;
+  exports.customTooltips = customTooltips;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
+//# sourceMappingURL=custom-tooltips.js.map
+
+
+/***/ }),
+
+/***/ "./node_modules/ngx-bootstrap/collapse/fesm5/ngx-bootstrap-collapse.js":
+/*!*****************************************************************************!*\
+  !*** ./node_modules/ngx-bootstrap/collapse/fesm5/ngx-bootstrap-collapse.js ***!
+  \*****************************************************************************/
+/*! exports provided: CollapseDirective, CollapseModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CollapseDirective", function() { return CollapseDirective; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CollapseModule", function() { return CollapseModule; });
+/* harmony import */ var _angular_animations__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/animations */ "./node_modules/@angular/animations/fesm5/animations.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/** @type {?} */
+var COLLAPSE_ANIMATION_TIMING = '400ms cubic-bezier(0.4,0.0,0.2,1)';
+/** @type {?} */
+var expandAnimation = [
+    Object(_angular_animations__WEBPACK_IMPORTED_MODULE_0__["style"])({ height: 0, visibility: 'hidden' }),
+    Object(_angular_animations__WEBPACK_IMPORTED_MODULE_0__["animate"])(COLLAPSE_ANIMATION_TIMING, Object(_angular_animations__WEBPACK_IMPORTED_MODULE_0__["style"])({ height: '*', visibility: 'visible' }))
+];
+/** @type {?} */
+var collapseAnimation = [
+    Object(_angular_animations__WEBPACK_IMPORTED_MODULE_0__["style"])({ height: '*', visibility: 'visible' }),
+    Object(_angular_animations__WEBPACK_IMPORTED_MODULE_0__["animate"])(COLLAPSE_ANIMATION_TIMING, Object(_angular_animations__WEBPACK_IMPORTED_MODULE_0__["style"])({ height: 0, visibility: 'hidden' }))
+];
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+var CollapseDirective = /** @class */ (function () {
+    function CollapseDirective(_el, _renderer, _builder) {
+        this._el = _el;
+        this._renderer = _renderer;
+        /**
+         * This event fires as soon as content collapses
+         */
+        this.collapsed = new _angular_core__WEBPACK_IMPORTED_MODULE_1__["EventEmitter"]();
+        /**
+         * This event fires when collapsing is started
+         */
+        this.collapses = new _angular_core__WEBPACK_IMPORTED_MODULE_1__["EventEmitter"]();
+        /**
+         * This event fires as soon as content becomes visible
+         */
+        this.expanded = new _angular_core__WEBPACK_IMPORTED_MODULE_1__["EventEmitter"]();
+        /**
+         * This event fires when expansion is started
+         */
+        this.expands = new _angular_core__WEBPACK_IMPORTED_MODULE_1__["EventEmitter"]();
+        // shown
+        this.isExpanded = true;
+        // hidden
+        this.isCollapsed = false;
+        // stale state
+        this.isCollapse = true;
+        // animation state
+        this.isCollapsing = false;
+        /**
+         * turn on/off animation
+         */
+        this.isAnimated = false;
+        this._display = 'block';
+        this._stylesLoaded = false;
+        this._COLLAPSE_ACTION_NAME = 'collapse';
+        this._EXPAND_ACTION_NAME = 'expand';
+        this._factoryCollapseAnimation = _builder.build(collapseAnimation);
+        this._factoryExpandAnimation = _builder.build(expandAnimation);
+    }
+    Object.defineProperty(CollapseDirective.prototype, "display", {
+        set: /**
+         * @param {?} value
+         * @return {?}
+         */
+        function (value) {
+            if (!this.isAnimated) {
+                this._renderer.setStyle(this._el.nativeElement, 'display', value);
+                return;
+            }
+            this._display = value;
+            if (value === 'none') {
+                this.hide();
+                return;
+            }
+            this.show();
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(CollapseDirective.prototype, "collapse", {
+        get: /**
+         * @return {?}
+         */
+        function () {
+            return this.isExpanded;
+        },
+        /** A flag indicating visibility of content (shown or hidden) */
+        set: /**
+         * A flag indicating visibility of content (shown or hidden)
+         * @param {?} value
+         * @return {?}
+         */
+        function (value) {
+            if (!this._player || this._isAnimationDone) {
+                this.isExpanded = value;
+                this.toggle();
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    /**
+     * @return {?}
+     */
+    CollapseDirective.prototype.ngAfterViewChecked = /**
+     * @return {?}
+     */
+    function () {
+        this._stylesLoaded = true;
+        if (!this._player || !this._isAnimationDone) {
+            return;
+        }
+        this._player.reset();
+        this._renderer.setStyle(this._el.nativeElement, 'height', '*');
+    };
+    /** allows to manually toggle content visibility */
+    /**
+     * allows to manually toggle content visibility
+     * @return {?}
+     */
+    CollapseDirective.prototype.toggle = /**
+     * allows to manually toggle content visibility
+     * @return {?}
+     */
+    function () {
+        if (this.isExpanded) {
+            this.hide();
+        }
+        else {
+            this.show();
+        }
+    };
+    /** allows to manually hide content */
+    /**
+     * allows to manually hide content
+     * @return {?}
+     */
+    CollapseDirective.prototype.hide = /**
+     * allows to manually hide content
+     * @return {?}
+     */
+    function () {
+        var _this = this;
+        this.isCollapsing = true;
+        this.isExpanded = false;
+        this.isCollapsed = true;
+        this.isCollapsing = false;
+        this.collapses.emit(this);
+        this._isAnimationDone = false;
+        this.animationRun(this.isAnimated, this._COLLAPSE_ACTION_NAME)((/**
+         * @return {?}
+         */
+        function () {
+            _this._isAnimationDone = true;
+            _this.collapsed.emit(_this);
+            _this._renderer.setStyle(_this._el.nativeElement, 'display', 'none');
+        }));
+    };
+    /** allows to manually show collapsed content */
+    /**
+     * allows to manually show collapsed content
+     * @return {?}
+     */
+    CollapseDirective.prototype.show = /**
+     * allows to manually show collapsed content
+     * @return {?}
+     */
+    function () {
+        var _this = this;
+        this._renderer.setStyle(this._el.nativeElement, 'display', this._display);
+        this.isCollapsing = true;
+        this.isExpanded = true;
+        this.isCollapsed = false;
+        this.isCollapsing = false;
+        this.expands.emit(this);
+        this._isAnimationDone = false;
+        this.animationRun(this.isAnimated, this._EXPAND_ACTION_NAME)((/**
+         * @return {?}
+         */
+        function () {
+            _this._isAnimationDone = true;
+            _this.expanded.emit(_this);
+        }));
+    };
+    /**
+     * @param {?} isAnimated
+     * @param {?} action
+     * @return {?}
+     */
+    CollapseDirective.prototype.animationRun = /**
+     * @param {?} isAnimated
+     * @param {?} action
+     * @return {?}
+     */
+    function (isAnimated, action) {
+        var _this = this;
+        if (!isAnimated || !this._stylesLoaded) {
+            return (/**
+             * @param {?} callback
+             * @return {?}
+             */
+            function (callback) { return callback(); });
+        }
+        this._renderer.setStyle(this._el.nativeElement, 'overflow', 'hidden');
+        this._renderer.addClass(this._el.nativeElement, 'collapse');
+        /** @type {?} */
+        var factoryAnimation = (action === this._EXPAND_ACTION_NAME)
+            ? this._factoryExpandAnimation
+            : this._factoryCollapseAnimation;
+        if (this._player) {
+            this._player.destroy();
+        }
+        this._player = factoryAnimation.create(this._el.nativeElement);
+        this._player.play();
+        return (/**
+         * @param {?} callback
+         * @return {?}
+         */
+        function (callback) { return _this._player.onDone(callback); });
+    };
+    CollapseDirective.decorators = [
+        { type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["Directive"], args: [{
+                    selector: '[collapse]',
+                    exportAs: 'bs-collapse',
+                    host: {
+                        '[class.collapse]': 'true'
+                    }
+                },] }
+    ];
+    /** @nocollapse */
+    CollapseDirective.ctorParameters = function () { return [
+        { type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["ElementRef"] },
+        { type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["Renderer2"] },
+        { type: _angular_animations__WEBPACK_IMPORTED_MODULE_0__["AnimationBuilder"] }
+    ]; };
+    CollapseDirective.propDecorators = {
+        collapsed: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["Output"] }],
+        collapses: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["Output"] }],
+        expanded: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["Output"] }],
+        expands: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["Output"] }],
+        isExpanded: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["HostBinding"], args: ['class.in',] }, { type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["HostBinding"], args: ['class.show',] }, { type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["HostBinding"], args: ['attr.aria-expanded',] }],
+        isCollapsed: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["HostBinding"], args: ['attr.aria-hidden',] }],
+        isCollapse: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["HostBinding"], args: ['class.collapse',] }],
+        isCollapsing: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["HostBinding"], args: ['class.collapsing',] }],
+        display: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["Input"] }],
+        isAnimated: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["Input"] }],
+        collapse: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["Input"] }]
+    };
+    return CollapseDirective;
+}());
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+var CollapseModule = /** @class */ (function () {
+    function CollapseModule() {
+    }
+    /**
+     * @return {?}
+     */
+    CollapseModule.forRoot = /**
+     * @return {?}
+     */
+    function () {
+        return { ngModule: CollapseModule, providers: [] };
+    };
+    CollapseModule.decorators = [
+        { type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"], args: [{
+                    declarations: [CollapseDirective],
+                    exports: [CollapseDirective]
+                },] }
+    ];
+    return CollapseModule;
+}());
+
+
+//# sourceMappingURL=ngx-bootstrap-collapse.js.map
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=common.js.map
